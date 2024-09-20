f31e193d00c98a3b126a124de1260a2ff1fbcdeb
Michael <mikhail6886@yandex.ru>
2024-09-20 16:05:29 +0500

    added first test

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,59 @@
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+
+public class SimpleJUnitTest {
+//    @Test
+//    void successfulSearchTest () {
+//        Selenide.open("https://www.google.com/");
+//        $("[name=q]").setValue("selenide").pressEnter();
+//        $("[id=search]").shouldHave(text("https://selenide.org/"));
+//    }
+    int result;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("\n### beforeAll()\n");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("###   beforeEach()");
+        result = getResult();
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("###   afterEach()\n");
+        result = 0;
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("### afterAll()\n");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("### firstTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("### secondTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("### thirdTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    private int getResult() {
+        return 3;
+    }
+}
