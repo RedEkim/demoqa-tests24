591fd28fc793e3a9eb3f09b9d41f485ecdb52f70
Michael <mikhail6886@yandex.ru>
2024-10-17 15:17:44 +0500

    refactoring

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -12,47 +12,36 @@ import static com.codeborne.selenide.Selenide.*;
 public class RegistrationTests extends TestBase {
 
     @Test
-    void fillFormTest() {
+    void RegistrationWithPageObjectsTests() {
         open("/automation-practice-form");
         $("#firstName").setValue("Mike");
         $("#lastName").setValue("Mikov");
         $("#userEmail").setValue(("mike@mail.com"));
-
         // radio buttons
         $("[for=gender-radio-1]").click();
         $("#userNumber").setValue("12345678910");
-
         // Calendar
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("January");
         $(".react-datepicker__year-select").selectOption("1970");
         $("[class=\"react-datepicker__day react-datepicker__day--001\"]").shouldHave(text("1")).click();
-
         // Subject
         $("#subjectsInput").setValue("en");
         $("#react-select-2-option-0").click();
-
-
-
         // checkboxes
         $("[for=hobbies-checkbox-1]").click();
         $("[for=hobbies-checkbox-3]").click();
-
         // upload picture
         $("#uploadPicture").uploadFile(new File("src/test/java/data/pngwing.png"));
-
         // address
         $("#currentAddress").setValue("Some Street 1");
-
         // dropdown state city
         $("#state").click();
         $("#react-select-3-option-2").click();
         $("#city").click();
         $("#react-select-4-option-0").click();
-
         // button submit
         $("button[type=submit]").click();
-
         // verify
         $x("//td[contains(text(), 'Student Name')]/../td[2]").shouldHave(text("Mike Mikov"));
         $x("//td[contains(text(), 'Student Email')]/../td[2]").shouldHave(text("mike@mail.com"));
