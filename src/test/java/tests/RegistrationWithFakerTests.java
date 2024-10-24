1ac81e117cb66300bbfa5de33e176eaa278de7d1
Michael <mikhail6886@yandex.ru>
2024-10-24 21:57:51 +0500

    refactoring the RegistrationWithFakerTests

--- src/test/java/tests/RegistrationWithFakerTests.java
+++ src/test/java/tests/RegistrationWithFakerTests.java
@@ -1,10 +1,14 @@
 package tests;
 
+import com.github.javafaker.DateAndTime;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
+import javax.xml.crypto.Data;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 import static utils.RandomUtils.getRandomGender;
@@ -22,9 +26,16 @@ public class RegistrationWithFakerTests extends TestBase {
     String gender = getRandomGender();
     String phoneNumberRu = getRandomPhoneRu();
 
+    String[] subjects = {"Math", "English"};
+    String[] hobbies = {"Sports", "Reading", "Music"};
+
+    String state = "NCR";
+    String city = "Delhi";
+
     @Test
     @DisplayName("Успешная регистрация с заполнением всех полей")
     void successfulRegistrationTest() {
+
         registrationPage.openPage("/automation-practice-form")
                         .setFirstName(firstName)
                         .setLastName(lastName)
@@ -32,29 +43,29 @@ public class RegistrationWithFakerTests extends TestBase {
                         .setGender(gender)
                         .setUserNumber(phoneNumberRu)
                         .setDateOfBirth("24", "October", "2024")
-                        .setSubject("Math")
-                        .setSubject("English")
-                        .setHobbie("Sports")
-                        .setHobbie("Reading")
-                        .setHobbie("Music")
+                        .setSubject(subjects[0])
+                        .setSubject(subjects[1])
+                        .setHobbie(hobbies[0])
+                        .setHobbie(hobbies[1])
+                        .setHobbie(hobbies[2])
                         .setPicture("src/test/java/resources/img/pngwing.png")
                         .setAddress(streetAddress)
-                        .setStateAndCity("NCR", "Delhi")
+                        .setStateAndCity(state, city)
                         .setSubmit();
 
 
         registrationPage.getModalDialogAppear()
                         .checkModalTitle("Thanks for submitting the form")
-                        .checkResult("Student Name", firstName + " " + lastName)
+                        .checkResult("Student Name", (firstName + " " + lastName))
                         .checkResult("Student Email", email)
                         .checkResult("Gender", gender)
                         .checkResult("Mobile", phoneNumberRu)
                         .checkResult("Date of Birth", "24 October,2024")
-                        .checkResult("Subjects", "Maths, English")
-                        .checkResult("Hobbies", "Sports, Reading, Music")
+                        .checkResult("Subjects", subjects[0])
+                        .checkResult("Hobbies", hobbies[0] + ", " + hobbies[1] + ", " + hobbies[2])
                         .checkResult("Picture", "pngwing.png")
                         .checkResult("Address", streetAddress)
-                        .checkResult("State and City", "NCR Delhi")
+                        .checkResult("State and City", state + " " + city)
                         .setCloseBtn();
     }
 
